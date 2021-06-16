249a1578abfa91f04578930a89677537ed834c94
Glitch (glitch-hello-website) <none>
2021-06-16 12:02:00 +0000

    🌵🌝 Checkpoint ./index.js:32649417/197

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require ("discord.js")
 const bot = new Discord.Client();
 
 bot.on('ready', () => {
+  bot.user.setActivity("Cubescom Özel | George O' Land")
+  type: "PLAYING"
   console.log(`${bot.user.tag} olarak giriş yapıldı!`);
 });
 
