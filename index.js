a7928bf58fc073639df537e14e48cf3561dfb5bf
Glitch (glitch-hello-website) <none>
2021-06-16 11:37:59 +0000

    🏄🕍 Checkpoint ./index.js:32649417/1146 ./.glitch-assets:32649417/482

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ bot.on('ready', () => {
   console.log(`${bot.user.tag} olarak giriş yapıldı!`);
 });
 
+bot.on('message', message => {
+  if (message.content === 'how to embed') {
+    const embed = new MessageEmbed()
+      .setTitle('A slick little embed')
+      .setColor(0xff0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+  }
+});
 bot.on('message', msg => {
   if (msg.content === 'Yazı') {
     msg.reply('Cevap');
   }
 });
+bot.login(process.env.token);
